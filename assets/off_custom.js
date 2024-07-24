36d6c230ec884e0b1eb8bfe0a573df6c0f44c03d
OFFicina-github <digital@nextgroup.eu>
2024-07-24 15:15:04 +0200

    bundle

--- assets/off_custom.js
+++ assets/off_custom.js
@@ -461,8 +461,8 @@ if (selects.length > 0) {
 
 // Se bundle KIT allora whishlist in fixed
 setTimeout(function(){
-const element = document.querySelector('.thpb_combo_wflex .thpb_combo_title .thpb_combo_wflex');
-    if (element && element.textContent.includes('Questo KIT contiene')) {
+const titoloBundleElement = document.querySelector('.thpb_combo_wflex .thpb_combo_title .thpb_combo_wflex');
+    if (titoloBundleElement && titoloBundleElement.textContent.includes('Questo KIT contiene')) {
         querySelector('.swym-btn-container .swym-button').classList.add('attivo_fixed');
     }
 }, 3000);
